440d774c1a5aba31c99134690e86d239b4c7fee9
Dean Camera <dean@fourwalledcubicle.com>
2024-09-22 20:40:39 +1000

    Add skeleton project.

---
+++ RayTracer/Main.cpp
@@ -0,0 +1,48 @@
+﻿#include <SFML/Graphics.hpp>
+
+#include <vector>
+
+namespace
+{
+	constexpr auto kWidth = 800;
+	constexpr auto kHeight = 600;
+}
+
+int main(int argc, char* argv[])
+{
+	sf::RenderWindow window(sf::VideoMode(kWidth, kHeight), "RayTracer");
+
+	std::vector<sf::Uint8> pixels(kWidth * kHeight * 4);
+	memset(pixels.data(), 0xFF, pixels.size());
+
+	sf::Texture texture;
+	texture.create(kWidth, kHeight);
+
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+
+	while (window.isOpen())
+	{
+	    sf::Event event;
+	    while (window.pollEvent(event))
+	    {
+	        switch (event.type)
+	        {
+	            case sf::Event::Closed:
+	            {
+	                window.close();
+	                break;
+	            }
+
+	            default:
+	                break;
+			}
+		}
+
+		texture.update(pixels.data());
+
+		window.draw(sprite);
+
+		window.display();
+	}
+}
