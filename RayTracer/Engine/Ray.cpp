8b9cb4f0cc548095612331fdbd0d0a030f4f72c6
Dean Camera <dean@fourwalledcubicle.com>
2024-10-13 13:05:38 +1100

    Fix inverse direction calculations breaking when parallel to an axis.

--- RayTracer/Engine/Ray.cpp
+++ RayTracer/Engine/Ray.cpp
@@ -9,7 +9,7 @@
 Ray::Ray(const Vector& position, const Vector& direction)
 	: m_position(position)
 	, m_direction(direction)
-	, m_directionInverse(1 / m_direction.x(), 1 / m_direction.y(), 1 / m_direction.z())
+	, m_directionInverse(m_direction.x() ? 1 / m_direction.x() : 0, m_direction.y() ? 1 / m_direction.y() : 0, m_direction.z() ? 1 / m_direction.z() : 0)
 {
 	assert(direction.length() - 1 <= std::numeric_limits<double>::epsilon());
 }
