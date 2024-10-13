8abb9355c798942048c0958a174ef34dcbca6640
Dean Camera <dean@fourwalledcubicle.com>
2024-10-13 13:12:51 +1100

    Adjust default material properties.

--- RayTracer/Engine/Material.hpp
+++ RayTracer/Engine/Material.hpp
@@ -4,9 +4,9 @@ struct Material
 {
 public:
 	double	ambient = 0.1;
-	double	diffuse = 0.4;
-	double	specular = 0.9;
-	double	reflectivity = 0.1;
-	double	refractivity = 0.1;
+	double	diffuse = 0.3;
+	double	specular = 0.5;
+	double	reflectivity = 0.2;
+	double	refractivity = 0.0;
 	double	refractionIndex = 1.0;
 };
