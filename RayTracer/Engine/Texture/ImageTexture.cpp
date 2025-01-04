01b8eeabc1e8a33b894746a8b0a00b3b0a4e9539
Dean Camera <dean@fourwalledcubicle.com>
2025-01-04 15:36:25 +1100

    Only assign image texture pixels once the inputs have been validated.

--- RayTracer/Engine/Texture/ImageTexture.cpp
+++ RayTracer/Engine/Texture/ImageTexture.cpp
@@ -6,10 +6,11 @@
 
 ImageTexture::ImageTexture(size_t width, size_t height, const Color& multiplier, Interpolation interpolation, const uint32_t* pixels)
 	: Texture(width, height, multiplier, interpolation)
-	, m_pixels(&pixels[0], &pixels[width * height])
 {
 	if (! width || ! height || ! pixels)
 		throw std::runtime_error("Image texture create with invalid image parameters.");
+
+	m_pixels.assign(&pixels[0], &pixels[width * height]);
 }
 
 Color ImageTexture::colorAt(size_t x, size_t y) const
