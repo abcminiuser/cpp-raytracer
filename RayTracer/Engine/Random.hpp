12e85798bac5d6f28efc8b887ceed8ca8467366c
Dean Camera <dean@fourwalledcubicle.com>
2025-03-31 21:46:06 +1100

    Use simpler PRNG for random numbers for better performance.

--- RayTracer/Engine/Random.hpp
+++ RayTracer/Engine/Random.hpp
@@ -6,7 +6,7 @@ namespace Random
 {
 	static inline double SignedNormal()
 	{
-		thread_local std::mt19937 generator;
+		thread_local std::minstd_rand generator;
 		thread_local std::uniform_real_distribution<double> distribution(-1.0, 1.0);
 
 		return distribution(generator);
@@ -14,7 +14,7 @@ namespace Random
 
 	static inline double UnsignedNormal()
 	{
-		thread_local std::mt19937 generator;
+		thread_local std::minstd_rand generator;
 		thread_local std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
 		return distribution(generator);
