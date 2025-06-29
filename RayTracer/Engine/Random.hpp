71eff2025b55390012ccde434a50f8fceae71d8f
Dean Camera <dean@fourwalledcubicle.com>
2025-06-29 15:42:11 +1000

    Use Xoshiro for RNG operations, as it is faster than the standard library.

--- RayTracer/Engine/Random.hpp
+++ RayTracer/Engine/Random.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
+#include <XoshiroCpp.hpp>
+
 #include <random>
 
 namespace Random
 {
 	static inline double SignedNormal()
 	{
-		thread_local std::minstd_rand generator;
+		thread_local XoshiroCpp::Xoroshiro128PlusPlus generator;
 		thread_local std::uniform_real_distribution<double> distribution(-1.0, 1.0);
 
 		return distribution(generator);
@@ -14,7 +16,7 @@ namespace Random
 
 	static inline double UnsignedNormal()
 	{
-		thread_local std::minstd_rand generator;
+		thread_local XoshiroCpp::Xoroshiro128PlusPlus generator;
 		thread_local std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
 		return distribution(generator);
